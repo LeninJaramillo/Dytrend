bb177ee285419f6c09f7f1334fb2a616e461eff8
LeninJaramillo <lenin_jaramillo@hotmail.com>
2020-02-10 22:55:32 -0500

    new and create method for sales

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,5 +5,22 @@ class SalesController < ApplicationController
 
     def show
         @sale = Sale.find(params[:id])
+    end
+
+    def new
+        @sale = Sale.new
+    end
+
+    def create
+        @sale = Sale.new(post_params)
+        @sale.user_id = current_user.id
+        if @sale.save
+            redirect_to @sale
+            flash[:notice] = "GREAT your article is for sale!"
+        else
+        redirect_back(fallback_location: root_path)
+        flash[:alert] = "Something went wrong..! Try again"
+        end
+    end
 
 end
