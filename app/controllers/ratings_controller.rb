3bf4d2540a80a8a372885171d018a64c7d0029c1
LeninJaramillo <lenin_jaramillo@hotmail.com>
2020-02-14 13:38:36 -0500

    uploading to web

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,12 +4,19 @@ class RatingsController < ApplicationController
     end
 
     def create
-        @rating = Rating.new
-        @rating.user_buyer.id = current_user
+        @rating = Rating.new(rate_params)
+        @rating.user_buyer_id = current_user.id
+        @rating.user_seller_id = params[:user_id]
+       
         if @rating.save
-            redirect_to "/"
-            "/users/#{@rating.user_buyer.id}"
+            redirect_to "/users/#{@rating.user_seller_id}"
             flash[:notice] = "You have rated"
         end
-    def
+        puts @rating.errors
+    end
+
+    private
+    def rate_params
+        params.require(:rating).permit(:rating, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations"}
   root to: "sales#index"
   resources :sales
-  resources :users
-  resources :ratings
+  resources :users do 
+    resources :ratings
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
